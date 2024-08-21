71cb5750fa9c5cffeff8312c6f35bd2fff5eef12
LordofTech <chukwurahnnamdi81@gmail.com>
2024-08-21 20:56:30 +0100

    Update awsconfig.js

--- config/awsconfig.js
+++ config/awsconfig.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable no-undef */
-// config/awsConfig.js
+// config/awsconfig.js
 require('dotenv').config();
 const AWS = require('aws-sdk');
 
