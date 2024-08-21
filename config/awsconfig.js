15c90fa1d09260606ace35fd5f55c8fa1be1c75e
LordofTech <chukwurahnnamdi81@gmail.com>
2024-08-21 20:14:59 +0100

    Update awsconfig.js

--- config/awsconfig.js
+++ config/awsconfig.js
@@ -1,11 +1,13 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable no-undef */
+// config/awsConfig.js
+require('dotenv').config();
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  region: 'your-region',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-module.exports = dynamoDB;
+module.exports = AWS;
